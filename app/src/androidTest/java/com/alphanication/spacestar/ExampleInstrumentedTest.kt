00726aeec34d7f22171fb9f2fb0a9d9a33fa4e3f
vorobeye <russianfederation.vorobey@gmail.com>
2023-01-15 22:03:24 +0300

    rename package project

--- app/src/androidTest/java/com/alphanication/spacestar/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/alphanication/spacestar/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.spacestar
+package com.alphanication.spacestar
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.spacestar", appContext.packageName)
+        assertEquals("com.alphanication.spacestar", appContext.packageName)
     }
 }
