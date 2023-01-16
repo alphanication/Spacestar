29b1d14632d2a8421db7f6c99a76e894d96fae58
vorobeye <russianfederation.vorobey@gmail.com>
2023-01-16 07:14:19 +0300

    main activity base settings

--- app/src/main/java/com/alphanication/spacestar/ui/MainActivity.kt
+++ app/src/main/java/com/alphanication/spacestar/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.alphanication.spacestar
+package com.alphanication.spacestar.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.alphanication.spacestar.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
