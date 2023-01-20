c9779ce8a22fa24a093bc33f3e0e497a0d463508
vorobeye <russianfederation.vorobey@gmail.com>
2023-01-20 23:59:57 +0300

    resolve problems after rename modules presentation

--- feature_astronaut/feature_astronaut_presentation/src/main/java/com/alphanication/feature_astronaut_presentation/MainActivity.kt
+++ feature_astronaut/feature_astronaut_presentation/src/main/java/com/alphanication/feature_astronaut_presentation/MainActivity.kt
@@ -2,6 +2,7 @@ package com.alphanication.feature_astronaut_presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.alphanication.feature_astronaut.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
