39660918b60dc05510a3c71489ef406902f63dd2
vorobeye <russianfederation.vorobey@gmail.com>
2023-02-02 23:58:53 +0300

    add astronaut scope

---
+++ feature_astronaut/feature_astronaut_presentation/src/main/java/com/alphanication/feature_astronaut_presentation/di_helper/scope/AstronautScope.kt
@@ -0,0 +1,7 @@
+package com.alphanication.feature_astronaut_presentation.di_helper.scope
+
+import javax.inject.Scope
+
+@Scope
+@Retention(AnnotationRetention.RUNTIME)
+annotation class AstronautScope
