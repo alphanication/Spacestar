be4faaaf7d6dc9a7c4a85c6c7ecebaee7108d5f5
vorobeye <russianfederation.vorobey@gmail.com>
2023-02-04 23:37:57 +0300

    di updates

--- app/src/main/java/com/alphanication/spacestar/ui/fragments/navigation_splash_fragment/NavigationSplashFragment.kt
+++ app/src/main/java/com/alphanication/spacestar/ui/fragments/navigation_splash_fragment/NavigationSplashFragment.kt
@@ -1,5 +1,6 @@
-package com.alphanication.spacestar.ui.fragments.navigation_fragment
+package com.alphanication.spacestar.ui.fragments.navigation_splash_fragment
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,7 +8,8 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.alphanication.spacestar.R
 
-class NavigationFragment : Fragment() {
+@SuppressLint("CustomSplashScreen")
+class NavigationSplashFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
