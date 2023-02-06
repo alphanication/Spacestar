87c96161494340f13d41a8eb74fb6c1254cd0b58
vorobeye <russianfederation.vorobey@gmail.com>
2023-02-06 23:52:22 +0300

    splash screen xml

--- app/src/main/java/com/alphanication/spacestar/ui/fragments/navigation_splash_fragment/NavigationSplashFragment.kt
+++ app/src/main/java/com/alphanication/spacestar/ui/fragments/navigation_splash_fragment/NavigationSplashFragment.kt
@@ -1,5 +1,7 @@
 package com.alphanication.spacestar.ui.fragments.navigation_splash_fragment
 
+import android.os.Bundle
+import android.view.View
 import com.alphanication.spacestar.databinding.FragmentNavigationSplashBinding
 import com.alphanication.spacestar_base.ui.fragment.BaseFragment
 import javax.inject.Inject
@@ -13,4 +15,8 @@ class NavigationSplashFragment :
     override lateinit var factory: NavigationSplashViewModel.Factory
 
     override val viewModel: NavigationSplashViewModel by assistedViewModel()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
