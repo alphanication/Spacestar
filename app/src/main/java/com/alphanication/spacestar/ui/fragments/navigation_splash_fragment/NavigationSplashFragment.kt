3a8a49e1093289831136ea21251d54243dc2d437
vorobeye <russianfederation.vorobey@gmail.com>
2023-02-08 23:52:32 +0300

    deep links to feature_astronaut module

--- app/src/main/java/com/alphanication/spacestar/ui/fragments/navigation_splash_fragment/NavigationSplashFragment.kt
+++ app/src/main/java/com/alphanication/spacestar/ui/fragments/navigation_splash_fragment/NavigationSplashFragment.kt
@@ -1,10 +1,18 @@
 package com.alphanication.spacestar.ui.fragments.navigation_splash_fragment
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.alphanication.spacestar.databinding.FragmentNavigationSplashBinding
 import com.alphanication.spacestar_base.ui.fragment.BaseFragment
+import com.alphanication.spacestar_navigation.NavigationDeepLinks
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import javax.inject.Inject
+
+private const val DELAY_SPLASH_SCREEN = 1000L
 
 class NavigationSplashFragment :
     BaseFragment<NavigationSplashViewModel, FragmentNavigationSplashBinding>(
@@ -18,5 +26,16 @@ class NavigationSplashFragment :
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        navigateToAstronautFeatureModule()
+
+    }
+
+    private fun navigateToAstronautFeatureModule() = lifecycleScope.launch {
+        delay(DELAY_SPLASH_SCREEN)
+        findNavController().navigate(
+            Uri.parse(
+                NavigationDeepLinks.FEATURE_ASTRONAUT_MAIN_DEEP_LINK
+            )
+        )
     }
 }
---
+++ spacestar_navigation/src/main/java/com/alphanication/spacestar_navigation/NavigationDeepLinks.kt
@@ -0,0 +1,5 @@
+package com.alphanication.spacestar_navigation
+
+object NavigationDeepLinks {
+    const val FEATURE_ASTRONAUT_MAIN_DEEP_LINK = "spacestar://feature_astronaut"
+}
