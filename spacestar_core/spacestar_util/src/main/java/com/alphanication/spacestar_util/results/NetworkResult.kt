015894a4c42e5822aa5a083e02403e24f85e730c
vorobeye <russianfederation.vorobey@gmail.com>
2023-02-15 23:18:05 +0300

    add new spacestar base util module

---
+++ spacestar_core/spacestar_util/src/main/java/com/alphanication/spacestar_util/results/NetworkResult.kt
@@ -0,0 +1,30 @@
+package com.alphanication.spacestar_util.results
+
+import java.io.IOException
+
+enum class StatusNetworkResult {
+    SUCCESS,
+    ERROR
+}
+
+data class NetworkResult<out T>(
+    val status: StatusNetworkResult,
+    val data: T? = null,
+    val errorThrowable: Throwable? = null
+) {
+    companion object {
+        fun <T> success(data: T? = null): NetworkResult<T> =
+            NetworkResult(StatusNetworkResult.SUCCESS, data)
+
+        fun <T> error(errorModel: Throwable, data: T? = null): NetworkResult<T> =
+            NetworkResult(StatusNetworkResult.ERROR, data, errorModel)
+    }
+}
+
+sealed class ErrorTypes : IOException() {
+
+    data class AnotherError(
+        val errorMessage: String,
+        val errorCode: String? = null
+    ) : ErrorTypes()
+}
